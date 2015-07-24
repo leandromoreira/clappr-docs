b14a7150d0c05ebbf7a728887f34a0ce8371c200
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-07-24 00:43:14 -0300

    adds documentation

--- api.js
+++ api.js
@@ -2,10 +2,23 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Events",
+        "HLS",
         "HTML5Video",
         "Playback"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "base",
+        "playback"
+    ],
+    "allModules": [
+        {
+            "displayName": "base",
+            "name": "base"
+        },
+        {
+            "displayName": "playback",
+            "name": "playback"
+        }
+    ]
 } };
 });
